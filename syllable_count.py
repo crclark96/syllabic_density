f01494cb1ba3761f11b2236a30de0a26f86d4220
Collin Clark <crclark96@gmail.com>
2018-02-11 14:23:29 -0500

    add cursory density calculation

---
+++ syllable_count.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import sys
+from curses.ascii import isdigit
+from nltk.corpus import cmudict
+
+d = cmudict.dict()
+def nsyl(word):
+    try:
+        return [len(list(y for y in x
+                         if isdigit(str(y[-1]))))
+                for x in d[word.lower()]]
+    except KeyError:
+        return [0]
+
+if __name__ == '__main__':
+    o = open('syllabic_density.txt','w')
+    syllables = []
+    for word in d.keys():
+        syllables.append((word,nsyl(word)[0]))
+
+    density = [(word,syllbs/float(len(word)))
+               for word,syllbs in syllables
+               if syllbs > 0]
+    
+    density.sort(cmp=lambda x,y: cmp(y[1],x[1]))
+        
+    for entry in density:
+        o.write(str(entry[0]) + ', ' + str(entry[1]/float(len(entry[0]))) + '\r\n')
+
+    
+
